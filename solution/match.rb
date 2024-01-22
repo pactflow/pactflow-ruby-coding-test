e3c4d2ec35ebfc07e62f9076c6577bd71befcb7e
Vishal Sadriya <vishalsadriya.tps@gmail.com>
2024-01-22 18:18:28 +0100

    final touch

--- solution/match.rb
+++ solution/match.rb
@@ -3,6 +3,7 @@
 # tennis match game
 class Match
   attr_reader :player1, :player2
+  attr_accessor :current_score_player1, :current_score_player2, :total_score_player1, :total_score_player2
 
   TENNIS_SCORE = %w[0 15 30 40].freeze
 
@@ -16,57 +17,42 @@ class Match
   end
 
   def point_won_by(player)
-    if player == player1
-      @current_score_player1 += 1
-    else
-      @current_score_player2 += 1
-    end
-
+    player == player1 ? self.current_score_player1 += 1 : self.current_score_player2 += 1
     check_game_result
   end
 
   def score
-    "#{total_score}, #{current_score}"
+    "#{total_score_player1}-#{total_score_player2}, #{current_score}"
   end
 
   private
 
   def check_game_result
-    if @current_score_player1 >= 4 && @current_score_player1 >= @current_score_player2 + 2
-      @total_score_player1 += 1
+    if current_score_player1 >= 4 && current_score_player1 >= @current_score_player2 + 2
+      self.total_score_player1 += 1
       reset_current_scores
-    elsif @current_score_player2 >= 4 && @current_score_player2 >= @current_score_player1 + 2
-      @total_score_player2 += 1
+    elsif current_score_player2 >= 4 && current_score_player2 >= @current_score_player1 + 2
+      self.total_score_player2 += 1
       reset_current_scores
     end
   end
 
   def current_score
-    if @current_score_player1 >= 3 && @current_score_player2 >= 3
-      handle_deuce_or_advantage
-    else
-      "#{TENNIS_SCORE[@current_score_player1]}-#{TENNIS_SCORE[@current_score_player2]}"
-    end
-  end
-
-  def total_score
-    "#{@total_score_player1}-#{@total_score_player2}"
+    current_score_player1 >= 3 && current_score_player2 >= 3 ? handle_deuce_or_advantage : "#{TENNIS_SCORE[current_score_player1]}-#{TENNIS_SCORE[current_score_player2]}"
   end
 
   def handle_deuce_or_advantage
-    if @current_score_player1 == @current_score_player2
-      'Deuce'
-    elsif (@current_score_player1 - @current_score_player2).abs == 1
-      "Advantage #{leading_player}"
-    end
+    return 'Deuce' if current_score_player1 == current_score_player2
+
+    "Advantage #{leading_player}" if (current_score_player1 - current_score_player2).abs == 1
   end
 
   def leading_player
-    @current_score_player1 > @current_score_player2 ? player1 : player2
+    current_score_player1 > current_score_player2 ? player1 : player2
   end
 
   def reset_current_scores
-    @current_score_player1 = @current_score_player2 = 0
+    self.current_score_player1 = self.current_score_player2 = 0
   end
 end
 
