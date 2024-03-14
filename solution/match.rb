e48b717aca3e7885e5be8f1bed1ef3d69f792f81
ashish <ashishbhagore.ab@gmail.com>
2024-03-14 13:06:03 +0530

    Optimize changes

--- solution/match.rb
+++ solution/match.rb
@@ -2,12 +2,9 @@ require "pry"
 
 class Match
   def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2
-    @player1_points = 0
-    @player2_points = 0
-    @player1_games = 0
-    @player2_games = 0
+    @player1, @player2 = player1, player2
+    @player1_points = @player2_points = 0
+    @player1_games = @player2_games = 0
     @is_tiebreak = false
   end
 
