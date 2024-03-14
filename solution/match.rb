f567ca2d80804b1e35769a00d2309fbe05f2e220
ashish <ashishbhagore.ab@gmail.com>
2024-03-14 12:15:33 +0530

    Game point calcualted

--- solution/match.rb
+++ solution/match.rb
@@ -21,13 +21,18 @@ class Match
 
   private
 
+  def score_mapping
+    { 0 => '0', 1 => '15', 2 => '30', 3 => '40' }
+  end
+
   def handle_regular_game(player)
     if player == @player1
       @player1_points += 1
     else
       @player2_points += 1
     end
-    puts @player1_points, @player2_points
+
+    check_game_result
   end
 
   def handle_tiebreak(player)
@@ -36,9 +41,36 @@ class Match
     else
       @player2_points += 1
     end
+
+    check_tiebreak_result
+  end
+
+  def check_game_result
+    if @player1_points >= 4 && (@player1_points - @player2_points) >= 2
+      @player1_games += 1
+    elsif @player2_points >= 4 && (@player2_points - @player1_points) >= 2
+      @player2_games += 1
+    elsif @player1_points >= 3 && @player2_points >= 3
+      if @player1_points == @player2_points
+        'Deuce'
+      elsif (@player1_points - @player2_points).abs == 1
+        "Advantage #{@player1_points > @player2_points ? @player1 : @player2}"
+      else
+        "#{score_mapping[@player1_points]}-#{score_mapping[@player2_points]}"
+      end
+    else
+      puts "#{score_mapping[@player1_points]}-#{score_mapping[@player2_points]}"
+    end
+  end
+
+  def check_tiebreak_result
   end
 end
 
 match = Match.new("player 1", "player 2")
 match.point_won_by("player 1")
+match.point_won_by("player 1")
+match.point_won_by("player 1")
 match.point_won_by("player 2")
+
+#output will be 40-15
