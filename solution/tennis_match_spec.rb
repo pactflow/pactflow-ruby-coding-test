306184d464f76c1af0580260095da0ca80387ce8
Richa Dubey <richadubey39@gmail.com>
2024-04-05 16:02:32 +0530

    Add changes for basic scoring

--- solution/tennis_match_spec.rb
+++ solution/tennis_match_spec.rb
@@ -1,19 +1,35 @@
 # spec/tennis_match_spec.rb
-require 'byebug'
 require_relative 'tennis_match'
 
 RSpec.describe TennisMatch do
-  it "creates an instance of TennisMatch" do
-    match = TennisMatch.new("player 1", "player 2")
-    expect(match).to be_an_instance_of(TennisMatch)
-  end
+  let(:match) { TennisMatch.new("player 1", "player 2") }
 
-  describe "#pointWonBy" do
-    it "increments the score correctly for both players" do
-      match = TennisMatch.new("player 1", "player 2")
+  describe "#score" do
+
+    it "returns the score after player 1 scores once" do
+      match.pointWonBy("player 1")
+      expect(match.score).to eq("0-0, 15-0")
+    end
+
+    it "returns the score after both players score once" do
       match.pointWonBy("player 1")
       match.pointWonBy("player 2")
       expect(match.score).to eq("0-0, 15-15")
     end
+
+    it "returns the score after player 1 scores twice" do
+      match.pointWonBy("player 1")
+      match.pointWonBy("player 1")
+      expect(match.score).to eq("0-0, 30-0")
+    end
+
+    it "returns the score after player 1 scores thrice" do
+      match.pointWonBy("player 1")
+      match.pointWonBy("player 1")
+      match.pointWonBy("player 1")
+      expect(match.score).to eq("0-0, 40-0")
+    end
+
+    
   end
 end
