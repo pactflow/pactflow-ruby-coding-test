e2e66d134304afb13ca9c2f7e25b773abce7ca9c
Richa Dubey <richadubey39@gmail.com>
2024-04-09 17:16:37 +0530

    Add 2 test cases

--- solution/match.rb
+++ solution/match.rb
@@ -1,23 +1,48 @@
 require_relative 'tennis_match'
 
-# Input sequence to match the provided Java code
+# Ruby test cases sequence that are similar to those used for testing Java interfaces.
 match = TennisMatch.new("player 1", "player 2")
-# puts match.score # Output: "0-0"
+   puts "Ruby test cases sequence that are similar to those used for testing Java interfaces."
+	puts match.score # Output: "0-0"
 
-match.pointWonBy("player 1")
-match.pointWonBy("player 2")
-puts match.score # Output: "0-0, 15-15"
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 2")
+	puts match.score # Output: "0-0, 15-15"
 
-match.pointWonBy("player 1")
-match.pointWonBy("player 1")
-puts match.score # Output: "0-0, 40-15"
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 1")
+	puts match.score # Output: "0-0, 40-15"
 
-match.pointWonBy("player 2")
-match.pointWonBy("player 2")
-puts match.score # Output: "0-0, Deuce"
+	match.pointWonBy("player 2")
+	match.pointWonBy("player 2")
+	puts match.score # Output: "0-0, Deuce"
 
-match.pointWonBy("player 1")
-puts match.score # Output: "0-0, Advantage player 1"
+	match.pointWonBy("player 1")
+	puts match.score # Output: "0-0, Advantage player 1"
 
-match.pointWonBy("player 1")
-puts match.score # Output: "1-0"
+	match.pointWonBy("player 1")
+	puts match.score # Output: "1-0"
+
+
+# # One Player Wins the Game with No Deuce:
+match = TennisMatch.new("player 1", "player 2")
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 1")
+  puts "One Player Wins the Game with No Deuce:"
+	puts match.score
+
+
+
+# One Player Wins the Game with Deuce:
+match = TennisMatch.new("player 1", "player 2")
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 2")
+	match.pointWonBy("player 2")
+	match.pointWonBy("player 1")
+	match.pointWonBy("player 1")
+	puts "One Player Wins the Game with Deuce:"
+  puts match.score
