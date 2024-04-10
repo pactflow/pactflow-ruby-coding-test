bec1be183971590b31cb8a9a18881d160a7dad42
snaina <s.naina009@gmail.com>
2024-04-10 18:33:06 +0530

    Optimize code.

--- solution/tennis_tournament/spec/tennis_match_spec.rb
+++ solution/tennis_tournament/spec/tennis_match_spec.rb
@@ -1,56 +1,58 @@
+# frozen_string_literal: true
+
 require './tennis_match.rb'
 
 RSpec.describe TennisMatch do
-  let(:match) { TennisMatch.new("Player A", "Player B") }
+  let(:match) { TennisMatch.new('Player A', 'Player B') }
 
-  describe "#initialize" do
-    context "with valid player names" do
-      it "does not raise an error" do
+  describe '#initialize' do
+    context 'with valid player names' do
+      it 'does not raise an error' do
         expect { match }.not_to raise_error
       end
     end
 
-    context "with empty player names" do
-      it "raises an ArgumentError" do
-        expect { TennisMatch.new("", "Player B") }.to raise_error(ArgumentError)
-        expect { TennisMatch.new("Player A", "") }.to raise_error(ArgumentError)
+    context 'with empty player names' do
+      it 'raises an ArgumentError' do
+        expect { TennisMatch.new('', 'Player B') }.to raise_error(ArgumentError)
+        expect { TennisMatch.new('Player A', '') }.to raise_error(ArgumentError)
       end
     end
   end
 
-  describe "#score" do
-    context "when the match is not over" do
-      it "returns the set score" do
-        expect(match.score).to eq("Set score: Player A-Player B, match score: 0-0")
+  describe '#score' do
+    context 'when the match is not over' do
+      it 'returns the set score' do
+        expect(match.score).to eq('Set score: Player A-Player B, match score: 0-0')
       end
     end
 
-    context "when the match is over" do
-      it "returns the winner" do
-        match.instance_variable_set(:@match_winner, "Player A")
-        expect(match.score).to eq("Match over. Winner: Player A")
+    context 'when the match is over' do
+      it 'returns the winner' do
+        match.instance_variable_set(:@match_winner, 'Player A')
+        expect(match.score).to eq('Match over. Winner: Player A. Set score: Player A-Player B, match score: 0-0')
       end
     end
   end
 
-  describe "#point_won_by" do
-    context "with valid player name" do
-      it "increases the score of the player" do
-        match.point_won_by("Player A")
+  describe '#point_won_by' do
+    context 'with valid player name' do
+      it 'increases the score of the player' do
+        match.point_won_by('Player A')
         expect(match.instance_variable_get(:@player1_score)).to eq(1)
       end
     end
 
-    context "with invalid player name" do
-      it "raises an ArgumentError" do
-        expect { match.point_won_by("Unknown Player") }.to raise_error(ArgumentError)
+    context 'with invalid player name' do
+      it 'raises an ArgumentError' do
+        expect { match.point_won_by('Unknown Player') }.to raise_error(ArgumentError)
       end
     end
 
-    context "when the match is already over" do
-      it "raises a StandardError" do
-        match.instance_variable_set(:@match_winner, "Player A")
-        expect { match.point_won_by("Player A") }.to raise_error(StandardError)
+    context 'when the match is already over' do
+      it 'raises a StandardError' do
+        match.instance_variable_set(:@match_winner, 'Player A')
+        expect { match.point_won_by('Player A') }.to raise_error(StandardError)
       end
     end
   end
--- solution/tennis_tournament/tennis_match.rb
+++ solution/tennis_tournament/tennis_match.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'match_scoring'
+# Tennis match classs.
 class TennisMatch
+  include MatchScoring
   attr_reader :player1_name, :player2_name, :match_winner
 
   # Initialize the player names, default scores (match score as well as player scores).
   def initialize(player1_name, player2_name)
     # Raise an ArgumentError if player names are empty
-    raise ArgumentError, "Player names cannot be empty" if player1_name.empty? || player2_name.empty?
+    raise ArgumentError, 'Player names cannot be empty' if player1_name.empty? || player2_name.empty?
 
     @player1_name = player1_name
     @player2_name = player2_name
@@ -13,29 +18,14 @@ class TennisMatch
     @match_winner = nil
   end
 
-  # Returns current score of the match with players scores
-  # If there is no match winner yet:
-  #   - Concatenates player scores to display the current match score.
-  # If there is a match winner:
-  #   - Indicates that the match is over and displays the winner.
-  def score
-    if @match_winner.nil?
-      # Concatenate player match scores to display the current match score.
-      match_score = "#{@player1_score}-#{@player2_score}"
-      return "Set score: #{@player1_name}-#{@player2_name}, match score: #{match_score}"
-    else
-      match_score = "#{@player1_score}-#{@player2_score}"
-      return "Match over. Winner: #{@match_winner}. Set score: #{@player1_name}-#{@player2_name}, match score: #{match_score}"
-    end
-  end
-
   # Records a point won by a player and checks if the match is over.
   # If there is no match winner yet:
   #   - Increments the score of the respective player if the match is still ongoing.
   #   - After updating the scores, checks if there is a match winner.
   # If there is a match winner, no further points can be recorded.
   def point_won_by(player_name)
-    raise ArgumentError, "Invalid player name" unless [player1_name, player2_name].include?(player_name)
+    raise ArgumentError, 'Invalid player name' if !present?(player1_name) || !present?(player2_name)
+
     # raise StandardError, "Match already over" unless @match_winner.nil?
 
     update_score(player_name)
@@ -43,6 +33,10 @@ class TennisMatch
   end
 
   private
+
+  def present?(obj)
+    !obj.nil? && !obj.empty?
+  end
 
   # Check which player won the point and update their score accordingly
   def update_score(player_name)
@@ -54,8 +48,9 @@ class TennisMatch
   end
 end
 
+# Check match status.
 class Match < TennisMatch
-  MATCH_SCORE = { 0 => '0', 1 => '15', 2 => '30', 3 => '40' }
+  # MATCH_SCORE = { 0 => '0', 1 => '15', 2 => '30', 3 => '40' }.freeze
 
   private
 
@@ -64,10 +59,10 @@ class Match < TennisMatch
     if (@player1_score >= 4 && @player1_score >= @player2_score + 2) ||
        (@player2_score >= 4 && @player2_score >= @player1_score + 2)
       # Determine the match winner based on scores.
-      @match_winner = (@player1_score > @player2_score) ? @player1_name : @player2_name
+      @match_winner = @player1_score > @player2_score ? @player1_name : @player2_name
     elsif @player1_score >= 3 && @player2_score >= 3 && @player1_score == @player2_score
       # Handle deuce situation.
-      @match_winner = "Deuce"
+      @match_winner = 'Deuce'
     elsif @player1_score >= 3 && @player2_score >= 3 && (@player1_score - @player2_score).abs == 1
       # Handle advantage situation.
       @match_winner = "Advantage #{@player1_score > @player2_score ? @player1_name : @player2_name}"
@@ -77,30 +72,30 @@ end
 
 # Example usage:
 
-#Create a new match instance with players "Player A" and "Player B".
-scorer = Match.new("Player A", "Player B")
-#Both players 'Player A' and 'Player B' won a point each.
-scorer.point_won_by("Player A")
+# Create a new match instance with players "Player A" and "Player B".
+scorer = Match.new('Player A', 'Player B')
+# Both players 'Player A' and 'Player B' won a point each.
+scorer.point_won_by('Player A')
 puts scorer.score # Output=> Set score: Player A-Player B, match score: 1-0
-scorer.point_won_by("Player B")
+scorer.point_won_by('Player B')
 puts scorer.score # Output=> Set score: Player A-Player B, match score: 1-1
 
-#The Player A wins three consecutive points to win the game.
-scorer.point_won_by("Player A")
-scorer.point_won_by("Player A")
-scorer.point_won_by("Player A")
+# The Player A wins three consecutive points to win the game.
+scorer.point_won_by('Player A')
+scorer.point_won_by('Player A')
+scorer.point_won_by('Player A')
 puts scorer.score # Output=> Match over. Winner: Player A. Set score: Player A-Player B, match score: 4-1
 
-#The Player B wins three consecutive points to tie the game.
-scorer.point_won_by("Player B")
-scorer.point_won_by("Player B")
-scorer.point_won_by("Player B")
+# The Player B wins three consecutive points to tie the game.
+scorer.point_won_by('Player B')
+scorer.point_won_by('Player B')
+scorer.point_won_by('Player B')
 puts scorer.score # Output=> Match over. Winner: Deuce. Set score: Player A-Player B, match score: 4-4
 
-#The Player A won another point, gaining advantage.
-scorer.point_won_by("Player A")
+# The Player A won another point, gaining advantage.
+scorer.point_won_by('Player A')
 puts scorer.score # Output=> Match over. Winner: Advantage Player A. Set score: Player A-Player B, match score: 5-4
 
-#The Player A won another point, winning the game and match.
-scorer.point_won_by("Player A")
+# The Player A won another point, winning the game and match.
+scorer.point_won_by('Player A')
 puts scorer.score # Output=> Match over. Winner: Player A. Set score: Player A-Player B, match score: 6-4
