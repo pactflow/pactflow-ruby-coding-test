c8d3b5d76d7d48257c3ba8ec1ccbfbe4ca81eff8
prateek0411999 <prateekshrmprateek@gmail.com>
2024-06-01 19:55:30 +0530

    comments updated

--- solution/player.rb
+++ solution/player.rb
@@ -7,20 +7,24 @@ class Player
       @games_won = 0
     end
   
+    # Reset scores to 0
     def reset_scores
       @score = 0
       @tie_break_score = 0
     end
   
+    # Increment score by 1
     def win_point
       @score += 1
     end
   
+    # Increment games won by 1 and reset scores
     def win_game
       @games_won += 1
       reset_scores
     end
   
+    # Increment tie break score by 1
     def win_tie_break_point
       @tie_break_score += 1
     end
