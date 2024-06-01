56bf8101c34387839b8b7ffbb78c834d475df264
prateek0411999 <prateekshrmprateek@gmail.com>
2024-06-01 20:28:07 +0530

    added basic methods and initialization for game

--- solution/game.rb
+++ solution/game.rb
@@ -1,2 +1,15 @@
 class Game
+    attr_reader :player1, :player2, :scoring_strategy, :winner
+
+    def initialize(player1, player2, scoring_strategy)
+        @player1 = player1
+        @player2 = player2
+        @scoring_strategy = scoring_strategy
+    end
+
+    def point_won_by(player)
+    end
+
+    def score
+    end
 end
