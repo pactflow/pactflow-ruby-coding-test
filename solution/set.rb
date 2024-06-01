025989c3ba91e8828498df0e8b60fdbe8de3f2e9
prateek0411999 <prateekshrmprateek@gmail.com>
2024-06-01 20:37:24 +0530

    implemented Set with games, corresponding methods

--- solution/set.rb
+++ solution/set.rb
@@ -1,3 +1,23 @@
+require_relative "./game.rb"
+require_relative "./scoring_strategy/regular_scoring_strategy"
+
 class Set
-    # set has_many  games 
+    attr_reader :games
+
+    def initialize(player1, player2)
+        @player1 = player1
+        @player2 = player2
+        @games = []
+        start_new_game
+    end
+
+    def start_new_game
+        @games << Game.new(@player1, @player2, RegularScoring.new)
+    end
+
+    def point_won_by(player) 
+    end
+    
+    def score
+    end
 end
