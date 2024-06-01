f7dcc4fec4d95d223a12180c1017573c42c12a4f
prateek0411999 <prateekshrmprateek@gmail.com>
2024-06-01 19:45:11 +0530

    added required methods in Player

--- solution/player.rb
+++ solution/player.rb
@@ -1,2 +1,23 @@
 class Player
+    attr_accessor :name, :score, :games_won
+  
+    def initialize(name)
+      @name = name
+      reset_scores
+      @games_won = 0
+    end
+  
+    def reset_scores
+      @score = 0
+    end
+  
+    def win_point
+      @score += 1
+    end
+  
+    def win_game
+      @games_won += 1
+      reset_scores
+    end
+  
 end
